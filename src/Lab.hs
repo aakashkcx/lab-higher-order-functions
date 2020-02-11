d74565e7e95cc33907527736b874a83a0b97bcb7
Aakash Kc <aakashkcx@gmail.com>
2020-02-11 04:46:42 +0000

    complete -permutations

--- src/Lab.hs
+++ src/Lab.hs
@@ -18,30 +18,46 @@ import Prelude hiding ( elem, maximum, intersperse, transpose
 -- Recursive and higher-order functions
 
 elem :: Eq a => a -> [a] -> Bool
-elem = undefined
+-- elem x y = not (filter ((== x)) y == [])
+-- elem x = foldr (\a b -> b || a == x) False
 
 maximum :: Ord a => [a] -> a
-maximum = undefined
+maximum = foldr1 max
 
 any :: (a -> Bool) -> [a] -> Bool
-any = undefined
+any p []     = False
+any p (x:xs) = p x || any p xs
+
+elem x = any ((==) x)
 
 all :: (a -> Bool) -> [a] -> Bool
-all = undefined
+all p []     = False
+all p [x]    = p x
+all p (x:xs) = p x && all p xs
 
 flip :: (a -> b -> c) -> b -> a -> c
-flip = undefined
+flip f a b = f b a
 
 takeWhile :: (a -> Bool) -> [a] -> [a]
-takeWhile = undefined
+takeWhile _ [] = []
+takeWhile p (x:xs)
+    | p x       = x : takeWhile p xs
+    | otherwise = []
 
 zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-zipWith = undefined
+zipWith f []     _      = []
+zipWith f _      []     = []
+zipWith f (x:xs) (y:ys) = f x y : zipWith f xs ys
 
 groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
-groupBy = undefined
+groupBy p []    = []
+groupBy p [x]   = []
+groupBy p (x:y:xs)
+    | p x y     = [x,y] : groupBy p xs
+    | otherwise =   [x] : groupBy p (y:xs)
 
 permutations :: Eq a => [a] -> [[a]]
-permutations = undefined
+permutations [] = [[]]
+permutations (x:xs) = undefined
 
 --------------------------------------------------------------------------------
